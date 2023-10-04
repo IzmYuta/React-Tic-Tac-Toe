aa8a080dce1916227d82a917aaf7c81497a04e24
IzmYuta <y.izumi@4nonome.com>
2023-10-04 19:20:26 +0900

    Arrow function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
     // squaresの配列をコピー
+    // これをしないと、squaresの配列自体が変更されてしまい、状態変化が検知されない
+    // また、要件でロールバック機能が必要になった場合にも、このようにコピーを作成しておくと、簡単に実装できる
     const newSquares = squares.slice();
     // クリックしたマス目にXを代入
     newSquares[i] = "X";
@@ -16,20 +18,20 @@ export default function Board() {
       <div className="board-row">
         {/* onSquareClick={handleClick(0)}のように呼び出すとうまく動作しない */}
         {/* これは、ボードのレンダリング時に、handleClick関数が呼び出されてしまい、状態変化によってレンダリングが無限に行われてしまうことが原因 */}
-
-        <Square value={squares[0]} onSquareClick={handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        {/* アロー関数を利用することで解決できる */}
+        <Square value={squares[0]} onSquareClick={()=> handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=> handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={()=> handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={handleClick} />
-        <Square value={squares[4]} onSquareClick={handleClick} />
-        <Square value={squares[5]} onSquareClick={handleClick} />
+        <Square value={squares[3]} onSquareClick={()=> handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={()=> handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={()=> handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={handleClick} />
-        <Square value={squares[7]} onSquareClick={handleClick} />
-        <Square value={squares[8]} onSquareClick={handleClick} />
+        <Square value={squares[6]} onSquareClick={()=> handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={()=> handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={()=> handleClick(8)} />
       </div>
 
     </React.Fragment>
