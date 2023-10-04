8b980eecbcab86d6774ba6363d36f5ebe62bb0c1
IzmYuta <y.izumi@4nonome.com>
2023-10-04 18:53:04 +0900

    Add click function

--- src/App.js
+++ src/App.js
@@ -3,31 +3,40 @@ import React, { useState } from "react";
 export default function Board() {
   // 各マスの状態を管理するための配列を作成
   const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    // squaresの配列をコピー
+    const newSquares = squares.slice();
+    // クリックしたマス目にXを代入
+    newSquares[i] = "X";
+    // squaresの配列を更新
+    setSquares(newSquares);
+  }
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
 
     </React.Fragment>
   );
 }
 
-function Square({ value }) {
+// Boardコンポーネントから直接Squareコンポーネントを変更することはできないため、onSquareClickという名前で、関数をpropsとして渡す
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square">
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
